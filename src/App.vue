52dd3fb7c59230ee0f5fef1b33d6fa6aeaccc4da
linbinglin <lbl2@meitu.com>
2018-12-15 20:53:49 +0800

    remove

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,6 @@
 <template>
   <div id="app">
-    <div class="GameScreen">
-      <canvas width="512" height="448" ref="canvas" class="canvas">A drawing of something.</canvas>
-    </div>
+    <canvas width="1000" height="600" ref="canvas" class="canvas">A drawing of something.</canvas>
   </div>
 </template>
 
@@ -26,7 +24,6 @@
   html, body {
     padding: 0;
     margin: 0;
-    background-color: #666600;
   }
 
   img {
@@ -39,19 +36,6 @@
 
   .canvas {
     background-color: #666;
-  }
-
-  .GameScreen {
-    position: absolute;
-    overflow: hidden;
-    width: 512px;
-    height: 448px;
-    left: 50%;
-    margin-left: -256px;
-    top: 50%;
-    margin-top: -224px;
-    border: #630 double 4px;
-    background: #000;
   }
 
 </style>
